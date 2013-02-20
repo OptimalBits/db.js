ae1de4cd17e917022592f86cca8a3fe58dabde05
Manuel Astudillo <manuel@optimalbits.com>
2013-02-20 10:01:14 +0100

    Initial implementation (Tested on Chrome 24 and FF 18)

---
+++ db.js
@@ -0,0 +1,298 @@
+/**
+  ds.js (c) OptimalBits 2012.
+  
+  A lightweight wrapper for the IndexedDB API.
+  
+  authors: manuel@optimalbits.com
+           
+  Licensed as MIT.
+  
+  TODO:
+  
+  Support for queryUsageAndQuota and requestQuota.
+  
+  https://developers.google.com/chrome/whitepapers/storage
+*/
+
+(function() {
+
+/**
+    Abstraction of IndexedDB into an easier to use API that follows 
+    nodejs conventions as much as possible.
+*/ 
+
+  var indexedDB = 
+    window.indexedDB || 
+    window.webkitIndexedDB || 
+    window.mozIndexedDB || 
+    window.msIndexedDB || 
+    window.oIndexedDB;
+
+  IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction,
+  IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange;
+  
+  IDBTransaction.READ_ONLY = IDBTransaction.READ_ONLY || 'readonly';
+  IDBTransaction.READ_WRITE = IDBTransaction.READ_WRITE || 'readwrite';
+  
+  var DBFactory = function (name, version, cb) {
+    var request;
+    if(typeof version === 'function'){
+      cb = version;
+      version = undefined;
+      request = indexedDB.open(name);
+    }else{
+      request = indexedDB.open(name, version);
+    }
+    
+    try{
+      request.onsuccess = function (e) {
+        cb(null, new DBWrapper(e.target.result));
+      };
+
+      request.onerror = function (e) {
+        cb(e);
+      };
+    }catch(e){
+      DBFactory.delete(name, function(err){
+        if(!err){
+          open(name, cb);
+        }else{
+          cb(err);
+        }
+      });
+    }
+    
+    // request.onsuccess = request.onupgradeneeded;
+  }
+  
+  DBFactory.delete = function(name, cb) {
+    var request = indexedDB.deleteDatabase(name);
+    
+    try{
+      request.onsuccess = function (event) {
+        cb();
+      }
+      request.onerror = function (event) {
+        cb(new Error("indexedDB.delete Error: " + event.message));
+      }
+      request.onblocked = function (event) {
+        cb(new Error("indexedDB.delete Error: " + event.message));
+      }
+    }catch(e){
+      cb(e)
+    } 
+  }
+
+  var DBWrapper = function (db) {
+    this.db = db;
+  }
+
+  DBWrapper.prototype = {
+    incVersion : function(cb){
+      var 
+        self = this,
+        request, 
+        succeeded = false,
+        version = parseFloat(self.db.version);
+
+      version = version === '' ? 0 : version + 1;
+    
+      if(self.db.setVersion){
+        request = self.db.setVersion(version);
+      }else{
+        self.db.close()
+        request = indexedDB.open(self.db.name, version);
+      }
+      request.onsuccess = function(e){
+        if(!succeeded){
+          succeeded = true;
+          cb();
+        }
+      }
+    
+      request.onupgradeneeded = function(e){
+        self.db = e.target.result;
+        if(!succeeded){
+          succeeded = true;
+          cb();
+        }
+      }
+      request.onblocked = request.onerror = function (e) {
+        cb(e);
+      }
+    },
+
+    getObjectStore : function (name, cb) {
+      var transactionType =  IDBTransaction.READ_WRITE;
+      var self = this;
+      if (self._hasStore(name)){
+        self._getStore(name, transactionType, function(store){
+          cb(null, new ObjectStore(store));
+        })
+      } else {
+        self.incVersion(function(err){
+          if(!err){
+            var store = self.db.createObjectStore(name, {}, false);
+            cb(err, new ObjectStore(store));
+          }else{
+            cb(err);
+          }
+        })
+      }
+    },
+
+    deleteObjectStore : function (name, cb) {
+      var self = this;
+
+      if (self.db.objectStoreNames.contains(name)) {
+        self.incVersion(function(err){
+          if(!err){
+            self.db.deleteObjectStore(name);
+          }
+          cb(err);
+        })
+      } else {
+        cb(new Error('Store not found'));
+      }
+    },
+    
+    clearObjectStore : function (name, cb) {
+      this.deleteObjectStore(name, cb);
+    },
+  
+    getAllObjects : function (storeName, cb) {
+      var keyRange = IDBKeyRange.lowerBound('');
+      this.queryStore(storeName, keyRange, null, cb);
+    },
+
+    // Returns an object upfilling keyValue = {key:value}
+    // For faster queries create indexes on the required keys.
+    getObjectWhere : function (storeName, keyPath, value, cb) {
+      // TODO: Implement
+    },
+    
+    _hasStore : function(name){
+      return this.db.objectStoreNames.contains(name);
+    },
+  
+    _getStore : function (name, transactionType, cb) {
+      if(this._hasStore(name)){
+        var transaction = this.db.transaction([name], transactionType);
+        cb(transaction.objectStore(name));
+      }else{
+        cb();
+      }
+    },
+  }
+  
+  var ObjectStore = function(store){
+    this.store = store;
+  }
+  
+  ObjectStore.prototype = {
+    setObject : function (key, object, cb) {
+      var request = this.store.put(object, key);
+      
+      request.onsuccess = function (e) {
+        cb();
+      };
+
+      request.onerror = function (e) {
+        cb(e);
+      }; 
+    },
+
+    getObject : function (key, cb) {
+      var
+        keyRange = IDBKeyRange.only(key),
+        request = this.store.openCursor(keyRange);
+
+      request.onsuccess = function (e) {
+        var result = e.target.result;
+
+        if (result) {
+          cb(null, result.value);
+        }else{
+          cb(new Error('Object not found'))
+        }
+      }
+      
+      request.onerror = function (err) {
+        cb(err);
+      };
+    },
+
+    updateObject : function (key, newObj, cb) {
+      var self = this;
+
+      // To guarantee atomicity, we should use db.version here.
+      self.getObject(key, function (err, obj) {
+        if (obj) {
+          for(var prop in newObj){
+            if(newObj.hasOwnProperty(prop)){
+              obj[prop] = newObj[prop];
+            }
+          }
+          self.setObject(key, obj, cb);
+        }else{
+          cb(err);
+        }
+      });
+    },
+    
+    deleteObject : function (key, cb) {
+      var request = this.store.delete(key);
+
+      request.onsuccess = function (e) {
+        cb()
+      };
+
+      request.onerror = function (e) {
+        cb(e)
+      };
+    },
+    
+    query : function (keyRange, filter, cb) { 
+      var 
+        cursorRequest = this.store.openCursor(keyRange),
+        results = {};
+      
+      if(!cb){
+        cb = filter;
+        filter = null;
+      }
+
+      cursorRequest.onsuccess = function (e) {
+        var result = e.target.result;
+        if (!result) {
+          cb(null, results);
+        }else{
+          if (filter) {
+            if (filter(result.value)) {
+              results[result.key] = result.value;
+            }
+          } else {
+            results[result.key] = result.value;
+          }
+          result['continue'](); //Hack to shut up IDE equal to result.continue()
+        }
+      };
+
+      cursorRequest.onerror = function (err) {
+        cb(err);
+      };
+    }
+  }
+  
+  // AMD define happens at the end for compatibility with AMD loaders
+  // that don't enforce next-turn semantics on modules.
+  if (typeof define === 'function' && define.amd) {
+    define(function() {
+      return DBFactory;
+    });
+  }else{
+    this.DBFactory = DBFactory;
+  }
+  
+}).call(this);
+
---
+++ test/test_db.js
@@ -0,0 +1,118 @@
+define(['db'], function(DBFactory) {
+
+  var text1 = "Lorem Ipsum",
+    text2 = "dolor sit amet",
+    text3 = "",
+
+    url1 = 'http://localhost:8080/fixtures/lorem.txt',
+    url2 = 'http://localhost:8080/fixtures/ipsum.txt',
+    url3 = 'http://localhost:8080/fixtures/long.txt',
+    url4 = 'http://localhost:8080/fixtures/asdf.txt',
+    url5 = 'http://localhost:8080/fixtures/qwer.txt',
+    url6 = 'http://localhost:8080/fixtures/dsgbv.txt';
+
+  for (var i = 0; i < 1000; i++) {
+    text3 += "a";
+  }
+
+  describe('Database', function() {
+    var db;
+    
+    before(function(done){
+      DBFactory.delete('test', function(err){
+        done();
+      });
+    });
+    
+    it('Create Database', function(done) {
+      DBFactory('test', function(err, database) {
+        expect(err).to.not.be.ok();
+        expect(database).to.be.an(Object);
+        db = database;
+        done();
+      });
+    });
+    
+    it('Get an object store', function(done) {
+      db.getObjectStore('test1', function(err, storage){
+        expect(err).to.not.be.ok();
+        expect(storage).to.be.an(Object);
+        done();
+      });
+    });
+
+    it('Set data in a object store', function(done) {
+      db.getObjectStore('animals', function(err, storage){
+        expect(err).to.not.be.ok();
+        expect(storage).to.be.an(Object);
+        storage.setObject('tiger', {legs: 4}, function(err){
+          expect(err).to.not.be.ok();
+          done();
+        });
+      });
+    });
+
+    it('Get data from a object store', function(done) {
+      db.getObjectStore('animals', function(err, storage){
+        storage.setObject('leopard', {legs: 4}, function(err){
+          expect(err).to.not.be.ok();
+          storage.getObject('leopard', function(err, leopard){
+            expect(err).to.not.be.ok();
+            expect(leopard).to.be.an(Object);
+            expect(leopard.legs).to.be(4);
+            done();
+          });
+        });
+      });
+    });
+    
+    it('Query data from a object store', function(done) {
+      db.getObjectStore('animals', function(err, storage){
+        storage.setObject('leopard', {legs: 4}, function(err){
+          expect(err).to.not.be.ok();
+          storage.query(IDBKeyRange.only("leopard"), function(err, collection){
+            expect(err).to.not.be.ok();
+            expect(collection).to.be.an(Object);
+            expect(collection.leopard).to.be.ok();
+            expect(collection.leopard.legs).to.be(4);
+            done();
+          });
+        });
+      });
+    });
+
+    it('Update an object in a object store', function(done) {
+      db.getObjectStore('animals', function(err, storage){
+        storage.setObject('monkey', {legs: 4}, function(err){
+          expect(err).to.not.be.ok();
+          storage.updateObject('monkey', {legs: 2}, function(err){
+            expect(err).to.not.be.ok();
+            storage.getObject('monkey', function(err, monkey){
+              expect(err).to.not.be.ok();
+              expect(monkey).to.be.an(Object);
+              expect(monkey.legs).to.be(2);
+              done();
+            });
+          });
+        });
+      });
+    });
+    
+    it('Delete an object from an object store', function(done) {
+      db.getObjectStore('animals', function(err, storage){
+        storage.setObject('spider', {legs: 4}, function(err){
+          expect(err).to.not.be.ok();
+          storage.deleteObject('spider', function(err){
+            expect(err).to.not.be.ok();
+            storage.getObject('spider', function(err, spider){
+              expect(err).to.be.ok();
+              expect(spider).to.not.be.ok();
+              done();
+            });
+          });
+        });
+      });
+    });
+  });
+});
+
